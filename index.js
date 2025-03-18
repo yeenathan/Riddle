71ee4f8fce39b8b918cca8a4374f76dcf9027b9c
yeenathan <yeenathan21@gmail.com>
2025-03-18 12:27:40 -0700

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+// app.use(express.urlencoded({extended:true}))
+
+app.get("/", (req, res) => {
+  res.render("index.ejs"); //static
+})
+
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+});
+
