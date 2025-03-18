a4e9d0d72b02d2f0bef572546cce6c64d8f2eed7
jacksonnsaini <jsaini34@my.bcit.ca>
2025-03-18 13:09:10 -0700

    adding some css and front end

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 
 app.set("view engine", "ejs");
 // app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   res.render("index.ejs"); //static
